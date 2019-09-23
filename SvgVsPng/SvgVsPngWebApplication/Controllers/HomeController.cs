0bcdfe73b61ffa3f5df8506259d1c43b50b89ef6
rimever <mot_siva_ordin@hotmail.com>
2019-09-23 17:46:13 +0900

    一つの画像を並べて表示するWebページを作成。

---
+++ SvgVsPng/SvgVsPngWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SvgVsPngWebApplication.Models;
+
+namespace SvgVsPngWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Png()
+        {
+            return View();
+        }
+        public IActionResult Svg()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
